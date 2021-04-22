48be4d66d66619fc387c1bf87ea21f6cb3976454
slavaMalenko <denouement20@yandex.ru>
2021-04-23 00:54:30 +0300

    small layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import vk from './img/vk.jpg';
+import user from './img/icon-user.png';
+import menu from './img/menu.png'
+import './css/reset.css';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="container">
+          <div className="header__cont">
+            <img className="header__cont-img" src={vk} alt="" />
+          </div>
+        </div>
       </header>
+
+      <div className="network">
+        <div className="container">
+          <div className="network__sections">
+            <div className="network__menu">
+              <ul>
+                <li><a href="#"><img src={user} alt="" />Моя страница</a></li>
+                <li><a href="#"><img src={menu} alt="" />Новости</a></li>
+                <li><a href="#"><img src={menu} alt="" />Мессенджер</a></li>
+                <li><a href="#"><img src={menu} alt="" />Друзья</a></li>
+                <li><a href="#"><img src={menu} alt="" />Сообщества</a></li>
+                <li><a href="#"><img src={menu} alt="" />Фотографии</a></li>
+                <li><a href="#"><img src={menu} alt="" />Музыка</a></li>
+                <li><a href="#"><img src={menu} alt="" />Видео</a></li>
+                <li><a href="#"><img src={menu} alt="" />Игры</a></li>
+                <li><a href="#"><img src={menu} alt="" />Закладки</a></li>
+                <li><a href="#"><img src={menu} alt="" />Файлы</a></li>
+                <li><a href="#"><img src={menu} alt="" />Реклама</a></li>
+              </ul>
+            </div>
+
+            <div className="network__content"></div>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
