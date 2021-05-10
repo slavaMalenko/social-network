9adeecb82dc6b885b1fbe19be99a59edd5da5971
slavaMalenko <denouement20@yandex.ru>
2021-05-10 20:59:11 +0300

    small visual changes

--- src/components/ContentMessenger/Dialogue/Dialogue.js
+++ src/components/ContentMessenger/Dialogue/Dialogue.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import DialogueCss from './Dialogue.module.css'
-import profile from './../../ContentProfile/Profile/ProfileInformation/Photo/img/profile__img.jpg'
+import profileAhtem from './img/ahtem.jpg';
+import profileElya from './img/elya.jpg';
+import profileSasha from './img/sasha.jpg';
+import profileVictor from './img/victor.jpg'
 import { NavLink } from 'react-router-dom';
 
 function Dialogue(props) {
+    debugger
 
     let path = "/messenger/" + props.id;
+    let photo = profileElya;
+
+    if (props.id === '1') photo = profileElya
+    else if (props.id === '2') photo = profileVictor
+    else if (props.id === '3') photo = profileSasha
+    else photo = profileAhtem
 
     return (
         <NavLink to={path} className={DialogueCss.content}>
-            <img className={DialogueCss.img} src={profile} />
+            <img className={DialogueCss.img} src={photo} />
 
             <div className={DialogueCss.messages}>
                 <p className={DialogueCss.name}>{props.name}</p>
