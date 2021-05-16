c2de0f1ef6e9a34a74db35727ac9e8302ac122f0
slavaMalenko <denouement20@yandex.ru>
2021-05-16 21:34:25 +0300

    installing the axios library, preparing to work with the library

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,16 +4,33 @@ import usersAhtem from './img/ahtem.jpg';
 import usersElya from './img/elya.jpg';
 import usersSasha from './img/sasha.jpg';
 import usersVictor from './img/victor.jpg';
+import * as axios from 'axios';
 
 function Users(props) {
 
     if (props.state.usersPage.usersData.length === 0) {
-        props.setUsers([
-            { id: 1, followed: true, name: "Эльвина Ряпова", status: "время весны", location: { country: "Russia", city: "Simferopol" } },
-            { id: 2, followed: false, name: "Виктор Соляник", status: "does not exist", location: { country: "Russia", city: "Simferopol" } },
-            { id: 3, followed: false, name: "Александр Нечаев", status: "", location: { country: "Russia", city: "Simferopol" } },
-            { id: 4, followed: true, name: "Ахтем Маратов", status: "", location: { country: "Russia", city: "Simferopol" } },
-        ])
+
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                debugger
+                props.setUsers(response.data.items)
+            })
+
+        // props.setUsers([
+        //     { id: 1, followed: true, name: "Эльвина Ряпова", status: "время весны", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 2, followed: false, name: "Виктор Соляник", status: "does not exist", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 3, followed: false, name: "Александр Нечаев", status: "", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 4, followed: true, name: "Ахтем Маратов", status: "", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 1, followed: true, name: "Эльвина Ряпова", status: "время весны", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 2, followed: false, name: "Виктор Соляник", status: "does not exist", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 3, followed: false, name: "Александр Нечаев", status: "", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 4, followed: true, name: "Ахтем Маратов", status: "", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 1, followed: true, name: "Эльвина Ряпова", status: "время весны", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 2, followed: false, name: "Виктор Соляник", status: "does not exist", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 3, followed: false, name: "Александр Нечаев", status: "", location: { country: "Россия", city: "Симферополь" } },
+        //     { id: 4, followed: true, name: "Ахтем Маратов", status: "", location: { country: "Россия", city: "Симферополь" } },
+        // ])
     }
 
     const usersElements = props.state.usersPage.usersData
@@ -22,8 +39,8 @@ function Users(props) {
             followed={user.followed}
             name={user.name}
             status={user.status}
-            country={user.location.country}
-            city={user.location.city}
+            // country={user.location.country}
+            // city={user.location.city}
             follow={props.follow}
             unfollow={props.unfollow}
         />)
@@ -41,12 +58,12 @@ function Users(props) {
 
 function User(props) {
 
-    let photo = usersElya;
+    let photo = usersVictor;
 
-    if (props.id === 1) photo = usersElya
-    else if (props.id === 2) photo = usersVictor
-    else if (props.id === 3) photo = usersSasha
-    else photo = usersAhtem
+    // if (props.id === 1) photo = usersElya
+    // else if (props.id === 2) photo = usersVictor
+    // else if (props.id === 3) photo = usersSasha
+    // else photo = usersAhtem
 
     return (
         <div className={UsersCss.user}>
@@ -58,7 +75,7 @@ function User(props) {
                 <div className={UsersCss.information}>
                     <p className={UsersCss.name}>{props.name}</p>
                     <p className={UsersCss.status}>{props.status}</p>
-                    <p className={UsersCss.location}>{props.country}, {props.city}</p>
+                    <p className={UsersCss.location}>{'props.country'}, {'props.city'}</p>
                 </div>
 
                 <div className={UsersCss.button_cont}>
