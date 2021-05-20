98d99c2cbdf7c9619609c87e19dd35548f8463d2
slavaMalenko <denouement20@yandex.ru>
2021-05-20 22:49:50 +0300

    adding a class user component

---
+++ src/components/Users/User.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import UsersCss from './Users.module.css';
+import usersVictor from './img/victor.jpg';
+
+class User extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.photo = usersVictor;
+    }
+
+    render() {
+        return (
+            <div className={UsersCss.user}>
+                <div className={UsersCss.user_img}>
+                    <img src={this.photo} className={UsersCss.img} />
+                </div>
+
+                <div className={UsersCss.content}>
+                    <div className={UsersCss.information}>
+                        <p className={UsersCss.name}>{this.props.name}</p>
+                        <p className={UsersCss.status}>{this.props.status}</p>
+                        <p className={UsersCss.location}>{'this.props.country'}, {'this.props.city'}</p>
+                    </div>
+
+                    <div className={UsersCss.button_cont}>
+                        {
+                            this.props.followed
+                                ? <button onClick={() => { this.props.unfollow(this.props.id) }} className={UsersCss.button_on}>Удалить из друзей</button>
+                                : <button onClick={() => { this.props.follow(this.props.id) }} className={UsersCss.button_off}>Добавить в друзья</button>
+                        }
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default User;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import UsersCss from './Users.module.css';
-import usersAhtem from './img/ahtem.jpg';
-import usersElya from './img/elya.jpg';
-import usersSasha from './img/sasha.jpg';
-import usersVictor from './img/victor.jpg';
 import * as axios from 'axios';
+import User from './User';
 
 function Users(props) {
 
@@ -18,14 +15,6 @@ function Users(props) {
             })
 
         // props.setUsers([
-        //     { id: 1, followed: true, name: "Эльвина Ряпова", status: "время весны", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 2, followed: false, name: "Виктор Соляник", status: "does not exist", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 3, followed: false, name: "Александр Нечаев", status: "", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 4, followed: true, name: "Ахтем Маратов", status: "", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 1, followed: true, name: "Эльвина Ряпова", status: "время весны", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 2, followed: false, name: "Виктор Соляник", status: "does not exist", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 3, followed: false, name: "Александр Нечаев", status: "", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 4, followed: true, name: "Ахтем Маратов", status: "", location: { country: "Россия", city: "Симферополь" } },
         //     { id: 1, followed: true, name: "Эльвина Ряпова", status: "время весны", location: { country: "Россия", city: "Симферополь" } },
         //     { id: 2, followed: false, name: "Виктор Соляник", status: "does not exist", location: { country: "Россия", city: "Симферополь" } },
         //     { id: 3, followed: false, name: "Александр Нечаев", status: "", location: { country: "Россия", city: "Симферополь" } },
@@ -52,40 +41,6 @@ function Users(props) {
             </div>
 
             {usersElements}
-        </div>
-    );
-}
-
-function User(props) {
-
-    let photo = usersVictor;
-
-    // if (props.id === 1) photo = usersElya
-    // else if (props.id === 2) photo = usersVictor
-    // else if (props.id === 3) photo = usersSasha
-    // else photo = usersAhtem
-
-    return (
-        <div className={UsersCss.user}>
-            <div className={UsersCss.user_img}>
-                <img src={photo} className={UsersCss.img} />
-            </div>
-
-            <div className={UsersCss.content}>
-                <div className={UsersCss.information}>
-                    <p className={UsersCss.name}>{props.name}</p>
-                    <p className={UsersCss.status}>{props.status}</p>
-                    <p className={UsersCss.location}>{'props.country'}, {'props.city'}</p>
-                </div>
-
-                <div className={UsersCss.button_cont}>
-                    {
-                        props.followed
-                            ? <button onClick={() => { props.unfollow(props.id) }} className={UsersCss.button_on}>Удалить из друзей</button>
-                            : <button onClick={() => { props.follow(props.id) }} className={UsersCss.button_off}>Добавить в друзья</button>
-                    }
-                </div>
-            </div>
         </div>
     );
 }
