d82c8fa4f73e35d78d7c25d7df6b824d972f2aa1
slavaMalenko <denouement20@yandex.ru>
2021-05-21 21:43:54 +0300

    refactoring a functional user component into a class one

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,46 +3,42 @@ import UsersCss from './Users.module.css';
 import * as axios from 'axios';
 import User from './User';
 
-function Users(props) {
+class Users extends React.Component {
 
-    if (props.state.usersPage.usersData.length === 0) {
+    constructor(props) {
+        super(props);
+    }
 
+    componentDidMount() {
         axios
             .get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
-                debugger
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             })
-
-        // props.setUsers([
-        //     { id: 1, followed: true, name: "Эльвина Ряпова", status: "время весны", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 2, followed: false, name: "Виктор Соляник", status: "does not exist", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 3, followed: false, name: "Александр Нечаев", status: "", location: { country: "Россия", city: "Симферополь" } },
-        //     { id: 4, followed: true, name: "Ахтем Маратов", status: "", location: { country: "Россия", city: "Симферополь" } },
-        // ])
     }
 
-    const usersElements = props.state.usersPage.usersData
-        .map(user => <User
-            id={user.id}
-            followed={user.followed}
-            name={user.name}
-            status={user.status}
-            // country={user.location.country}
-            // city={user.location.city}
-            follow={props.follow}
-            unfollow={props.unfollow}
-        />)
+    render() {
+        return (
+            <div className={UsersCss.container} >
+                <div className={UsersCss.top}>
+                    <div className={UsersCss.all_users}>Все пользователи</div>
+                </div>
 
-    return (
-        <div className={UsersCss.container}>
-            <div className={UsersCss.top}>
-                <div className={UsersCss.all_users}>Все пользователи</div>
+                {
+                    this.props.state.usersPage.usersData
+                        .map(user => <User
+                            key={user.id}
+                            id={user.id}
+                            followed={user.followed}
+                            name={user.name}
+                            status={user.status}
+                            follow={this.props.follow}
+                            unfollow={this.props.unfollow}
+                        />)
+                }
             </div>
-
-            {usersElements}
-        </div>
-    );
+        );
+    }
 }
 
 export default Users;
