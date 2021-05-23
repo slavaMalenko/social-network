498acc905da7aaac40e23739947b2d654869ce2e
slavaMalenko <denouement20@yandex.ru>
2021-05-23 17:02:56 +0300

    refactoring the container component of users

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,37 +1,31 @@
 import React from 'react';
 import UsersCss from './Users.module.css';
 
-class User extends React.Component {
+function User(props) {
+    return (
+        <div className={UsersCss.user}>
+            <div className={UsersCss.user_img}>
+                <img src={props.photo} className={UsersCss.img} />
+            </div>
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className={UsersCss.user}>
-                <div className={UsersCss.user_img}>
-                    <img src={this.props.photo} className={UsersCss.img} />
+            <div className={UsersCss.content}>
+                <div className={UsersCss.information}>
+                    <p className={UsersCss.name}>{props.name}</p>
+                    <p className={UsersCss.status}>{props.status}</p>
+                    <p className={UsersCss.location}>{'props.country'}, {'props.city'}</p>
                 </div>
 
-                <div className={UsersCss.content}>
-                    <div className={UsersCss.information}>
-                        <p className={UsersCss.name}>{this.props.name}</p>
-                        <p className={UsersCss.status}>{this.props.status}</p>
-                        <p className={UsersCss.location}>{'this.props.country'}, {'this.props.city'}</p>
-                    </div>
-
-                    <div className={UsersCss.button_cont}>
-                        {
-                            this.props.followed
-                                ? <button onClick={() => { this.props.unfollow(this.props.id) }} className={UsersCss.button_on}>Удалить из друзей</button>
-                                : <button onClick={() => { this.props.follow(this.props.id) }} className={UsersCss.button_off}>Добавить в друзья</button>
-                        }
-                    </div>
+                <div className={UsersCss.button_cont}>
+                    {
+                        props.followed
+                            ? <button onClick={() => { props.unfollow(props.id) }} className={UsersCss.button_on}>Удалить из друзей</button>
+                            : <button onClick={() => { props.follow(props.id) }} className={UsersCss.button_off}>Добавить в друзья</button>
+                    }
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
 
 export default User;
