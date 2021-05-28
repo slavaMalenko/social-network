45ecb13e6655934352cfce037f282226af0c7615
slavaMalenko <denouement20@yandex.ru>
2021-05-28 23:03:18 +0300

    creating reducer in header, cookie, login

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import { combineReducers, createStore } from "redux";
 import profileReducer from './profile-reducer';
 import dialoguesReducer from './dialogues-reducer';
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messengerPage: dialoguesReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer,
 });
 
 let store = createStore(reducers);
